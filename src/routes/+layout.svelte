4902af6d9d9b2006dc0c8610c812a31120e8df73
KungRaseri <me@kungraseri.ninja>
2023-02-14 19:16:13 -0800

    initial project commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">Red Syndicate</svelte:fragment>
+			<a href="/">Home</a>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="container mx-auto p-8 space-y-8">
+	<h1>Software and Website Development</h1>
+	<hr />
+	<section class="card p-4">
+		<h2>Services</h2>
+		<section>
+			<ul class="list bg-token">
+				<li class="list-item">Development</li>
+				<li class="list-item">Testing</li>
+			</ul>
+		</section>
+	</section>
+	<hr />
+</div>
